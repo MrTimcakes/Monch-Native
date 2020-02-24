4c43337ad0bad69fef0ab1507b8c16f5c3acf198
Timothy Adamson <MrTimcakes@gmail.com>
2020-02-24 10:56:55 +0000

    Added Stack Navigator

--- navigation/index.js
+++ navigation/index.js
@@ -1,19 +1,55 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation'
-import Initial from '../screens/Initial'
-import AuthNavigation from './AuthNavigation'
-import AppNavigation from './AppNavigation'
+// import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+// import Initial from '../screens/Initial'
+// import AuthNavigation from './AuthNavigation'
+// import AppNavigation from './AppNavigation'
 
-const SwitchNavigator = createSwitchNavigator(
-  {
-    Initial: Initial,
-    Auth: AuthNavigation,
-    App: AppNavigation
-  },
-  {
-    initialRouteName: 'Initial'
-  }
-)
+// const SwitchNavigator = createSwitchNavigator(
+//   {
+//     Initial: Initial,
+//     Auth: AuthNavigation,
+//     App: AppNavigation
+//   },
+//   {
+//     initialRouteName: 'Initial'
+//   }
+// )
 
-const AppContainer = createAppContainer(SwitchNavigator)
+// const AppContainer = createAppContainer(SwitchNavigator)
 
-export default AppContainer
+// export default AppContainer
+
+
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.navigator>
+    </NavigationContainer>
+  );
+}

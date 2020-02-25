8ce0675d5f688351099c4e3896e40f0c6c6d789e
Timothy Adamson <MrTimcakes@gmail.com>
2020-02-25 10:42:21 +0000

    Added Icons in Tab Bar

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,4 +1,4 @@
-const tintColor = '#2f95dc';
+const tintColor = '#5663FF';
 
 export default {
   tintColor,
--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { FontAwesome } from '@expo/vector-icons';
+import Colors from '../constants/Colors';
 
 import InventoryScreen from '../screens/Inventory';
 import RecipiesScreen from '../screens/Recipies';
@@ -10,10 +12,40 @@ import ProfileScreen from '../screens/Profile';
 import SettingsScreen from '../screens/Settings';
 
 const Tab = createBottomTabNavigator();
+const screenOptions=({ route }) => ({
+  tabBarIcon: ({ focused, color, size }) => {
+    let iconName;
+
+    switch(route.name){
+      case 'Inventory':
+        iconName = focused ? 'shopping-basket' : 'shopping-basket';
+        break;
+      case 'Recipies':
+        iconName = focused ? 'cutlery' : 'cutlery';
+        break;
+      case 'Feed':
+        iconName = focused ? 'feed' : 'feed';
+        break;
+      case 'Profile':
+        iconName = focused ? 'user' : 'user';
+        break;
+      default:
+        iconName = focused ? 'info-circle' : 'info-circle';
+    }
+
+    // You can return any component that you like here!
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  },
+})
+
+const tabBarOptions={
+  activeTintColor: Colors.tabIconSelected,
+  inactiveTintColor: Colors.tabIconDefault,
+}
 
 function TabContainer() {
   return (
-    <Tab.Navigator headerMode="none" initialRouteName="Recipies" >
+    <Tab.Navigator headerMode="none" initialRouteName="Recipies" screenOptions={screenOptions} tabBarOptions={tabBarOptions} >
       <Tab.Screen name="Inventory" component={InventoryScreen}/>
       <Tab.Screen name="Recipies" component={RecipiesScreen}/>
       <Tab.Screen name="Feed" component={FeedScreen}/>

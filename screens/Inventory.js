ae3cfd609e8a04fd397c437f13f4638b0d3d8261
Timothy Adamson <MrTimcakes@gmail.com>
2020-03-04 11:37:51 +0000

    First Inventory Implementation

--- screens/Inventory.js
+++ screens/Inventory.js
@@ -1,31 +1,61 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native-elements'
+import React, { useState } from 'react'
+import { View, Image, TouchableOpacity, FlatList, StyleSheet, Text, } from 'react-native';
 import { withFirebaseHOC } from '../utilities/Firebase'
+
+function InventoryItem( item ) {
+  return (
+    <TouchableOpacity style={InvItemStyles.ItemContainer} >
+      { item.image_front_thumb_url && <Image style={InvItemStyles.ProductImage} source={{uri: item.image_front_thumb_url}} /> }
+      <Text style={InvItemStyles.ProductName}>{item.product_name}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const InvItemStyles = StyleSheet.create({
+  ItemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  ProductName:{
+    //
+  },
+  ProductImage:{
+    width: 50,
+    height: 50,
+    marginLeft: 5,
+    marginRight: 5,
+  }
+})
 
 function InventoryScreen(props) {
 
-  const SignOut = async () => {
-    try {
-      await props.firebase.signOut()
-      props.navigation.navigate('Auth')
-    } catch (error) {
-      console.log(error)
+  const [InventoryData, SetInventoryData] = useState(
+      {Inventory:[]}
+    );
+
+  props.firebase.firestore().collection("inventories").doc(props.firebase.auth().currentUser.uid).onSnapshot(function(doc) {
+    if (doc.exists) {
+      SetInventoryData( doc.data() )
+    } else {
+      // doc.data() will be undefined in this case
+      console.log("No such document!");
     }
-  }
+  })
 
   return (
     <View style={styles.container}>
-        <Text>Monch Inventory</Text>
-        <Button
-          title='Signout'
-          onPress={SignOut}
-          titleStyle={{
-            color: '#F57C00'
-          }}
-          type='clear'
-        />
-      </View>
+      
+      <FlatList style={styles.listContainer}
+        data={InventoryData.Inventory}
+        renderItem={({ item }) => (
+          <InventoryItem {...item} />
+        )}
+        keyExtractor={item => item.code}
+      />
+    </View>
   );
 }
 
@@ -35,7 +65,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  listContainer:{
+    flex: 1,
+    width: '100%',
   }
 })
 

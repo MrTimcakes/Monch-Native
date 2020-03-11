9f0f3c24495b55f2648c08d903ef17ae86775e3e
Timothy Adamson <MrTimcakes@gmail.com>
2020-03-11 17:58:49 +0000

    Added Project Colours 🎨

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,12 @@
 const tintColor = '#5663FF';
 
 export default {
+  color1: 'rgba(35, 46, 209, 1)',
+  color2: 'rgba(86, 99, 255, 1)',
+  color3: 'rgba(110, 127, 170, 1)',
+  color4: 'rgba(34, 36, 85, 1)',
+  color5: 'rgba(34, 34, 34, 1)',
+
   tintColor,
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,

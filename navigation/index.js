36069b0dff4bfd1749d7a4bc00ef8a85bfee3ccd
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-03 16:49:14 +0100

    Add SafeAreaProvider

--- navigation/index.js
+++ navigation/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthNavigation from './AuthNavigation'
 import AppNavigation from './AppNavigation'
 
@@ -10,12 +11,14 @@ const Stack = createStackNavigator();
 
 export default function AppContainer() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="Initial" >
-        <Stack.Screen name="Initial" component={InitialScreen}/>
-        <Stack.Screen name="Auth" component={AuthNavigation}/>
-        <Stack.Screen name="App" component={AppNavigation}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator headerMode="none" initialRouteName="Initial" >
+          <Stack.Screen name="Initial" component={InitialScreen}/>
+          <Stack.Screen name="Auth" component={AuthNavigation}/>
+          <Stack.Screen name="App" component={AppNavigation}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }

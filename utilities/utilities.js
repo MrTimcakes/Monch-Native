158c712fee3083516b4a30baad9240ef050890c1
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-15 23:39:33 +0100

    Unique ID generator

---
+++ utilities/utilities.js
@@ -0,0 +1,9 @@
+const createID = () => {
+  return Array(16)
+    .fill(0)
+    .map(() => String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+    .join('') + 
+    Date.now().toString(24);
+}
+
+export { createID }

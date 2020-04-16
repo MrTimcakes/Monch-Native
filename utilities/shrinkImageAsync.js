fb274c6a6cb9bda85a530cd5b32698b2ff419168
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-16 21:52:23 +0100

    Revert "Utilities for image upload"
    This reverts commit f869f91cfa0320af7bef023d9574eb5846db193e.

--- utilities/shrinkImageAsync.js
+++
@@ -1,8 +0,0 @@
-import * as ImageManipulator from 'expo-image-manipulator';
-
-function reduceImageAsync(uri) {
-  return ImageManipulator.manipulateAsync(uri, [{ resize: { width: 512 } }], {
-    compress: 0.8,
-  });
-}
-export default reduceImageAsync;

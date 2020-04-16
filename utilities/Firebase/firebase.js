9371198766e9b686b41fcb2b5cf2d543a6666868
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-16 21:48:33 +0100

    Support uploading images

--- utilities/Firebase/firebase.js
+++ utilities/Firebase/firebase.js
@@ -3,6 +3,10 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
 
+import uuid from 'uuid';
+import shrinkImageAsync from '../shrinkImageAsync';
+import getUserInfo from '../getUserInfo';
+
 // Initialize Firebase if not already
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
@@ -33,6 +37,62 @@ const Firebase = {
       .collection('users')
       .doc(`${userData.uid}`)
       .set(userData)
+  },
+  // Helpers
+  get collection() {
+    return firebase.firestore().collection(collectionName);
+  },
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  },
+  get timestamp() {
+    return Date.now();
+  },
+  // uploadPhoto: (uri, uploadUri) => {
+  uploadPhoto(uri, uploadUri){
+    return new Promise(async (res, rej) => {
+      const response = await fetch(uri);
+      const blob = await response.blob();
+  
+      const ref = firebase.storage().ref(uploadUri);
+      const unsubscribe = ref.put(blob).on(
+        'state_changed',
+        state => {},
+        err => {
+          unsubscribe();
+          rej(err);
+        },
+        async () => {
+          unsubscribe();
+          const url = await ref.getDownloadURL();
+          res(url);
+        },
+      );
+    });
+  },
+  async uploadPhotoAsync(uri, collectionName, imgName = uuid.v4()) {
+    const path = `${collectionName}/${this.uid}/${imgName}.jpg`;
+    return this.uploadPhoto(uri, path);
+  },
+  async post({ desc, image: localUri }) {
+    try {
+      const { uri: reducedImage, width, height } = await shrinkImageAsync( localUri, );
+      const postID = uuid.v4();
+      const remoteUri = await this.uploadPhotoAsync(reducedImage, 'userPosts', postID);
+      firebase.firestore().collection('users').doc(this.uid).collection('posts').doc(postID).set({
+        description: desc,
+        authorUid: this.uid,
+        postId: postID,
+        timestamp: this.timestamp,
+        imageWidth: width,
+        imageHeight: height,
+        image: remoteUri,
+        user: getUserInfo(),
+        likes: 0,
+      });
+    } catch ({ message }) {
+      alert(message);
+    }
   },
 }
 

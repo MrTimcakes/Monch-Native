dafb31d6da8acdaff8c847c228009635bc38fd31
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-17 15:40:14 +0100

    Add JavascriptTimeAgo for date processing

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import AppContainer from './navigation'
 import Firebase, { FirebaseProvider } from './utilities/Firebase'
 
+import JavascriptTimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+JavascriptTimeAgo.locale(en)
+
 export default function App() {
   return (
     <FirebaseProvider value={Firebase}>

97ab09f9de32954bfc8d7a068b06627e48f8bfe4
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-17 15:43:19 +0100

    Removed unnecessary code

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { FontAwesome } from '@expo/vector-icons';
-import Colors from '../constants/Colors';
 
 import MonchTabBar from '../components/BottomTabBar';
 import EmptyComponent from '../components/EmptyComponent';
@@ -14,17 +11,13 @@ import FeedScreen from '../screens/Feed';
 import ProfileScreen from '../screens/Profile';
 import SettingsScreen from '../screens/Settings';
 
-import DevScreen from '../screens/Dev';
-import Login2 from '../screens/auth/Login.jsx';
-
 const Tab = createBottomTabNavigator();
-
 function TabContainer() {
   return (
     <Tab.Navigator tabBar={props => <MonchTabBar {...props}/>} >
       <Tab.Screen name="Inventory" component={InventoryScreen} />
       <Tab.Screen name="Recipies" component={RecipiesScreen} />
-      <Tab.Screen name="MULTIFUNC" component={EmptyComponent}/>
+      <Tab.Screen name="MULTIFUNC" component={EmptyComponent} />
       <Tab.Screen name="Feed" component={FeedScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
@@ -32,8 +25,7 @@ function TabContainer() {
 }
 
 const Drawer = createDrawerNavigator();
-
-export default function App() {
+function App() {
   return (
     <Drawer.Navigator headerMode="none" initialRouteName="App" >
       <Drawer.Screen name="App" component={TabContainer}/>
@@ -41,3 +33,5 @@ export default function App() {
     </Drawer.Navigator>
   );
 }
+
+export default App;

f430b4556c800b3e836d4ca3e9ad58df70205cf3
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-20 15:33:22 +0100

    Show post

--- screens/PostList.jsx
+++ screens/PostList.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
-import { FlatList, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View, Text, Image } from 'react-native';
 
-// import 
+import { withFirebaseHOC } from 'Monch/utilities/Firebase';
 
-export default function PostList(P){
+import Post from 'Monch/components/Post';
+
+function PostList({route, firebase}){
+  const [ post, setPost ] = useState(null);
+
+  useEffect(() => {
+    setPost( route.params?.post );
+  }, []);
+
+  // const FetchPost = (uid = uid, postId = postId) => {
+  //   firebase.firestore().collection("users").doc(uid).collection("posts").doc(postId).get().then(function(doc) {
+  //     if (doc.exists) {
+  //       setPost( doc.data() );
+  //     } else {
+  //       // doc.data() will be undefined in this case
+  //       console.log("No such document!");
+  //     }
+  //   }).catch(function(error) {
+  //       console.log("Error getting document:", error);
+  //   });
+  
+  // }
+
+  if(post){
+    return(
+      <ScrollView>
+        <Post item={post} />
+        <View style={{height:80}}></View>
+      </ScrollView>
+    ) 
+  }
+
   return (
-    <View>
-      <Text>Test: {P.route.params?.postId}</Text>
+    <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Loading</Text>
     </View>
-    // <FlatList
-    //   contentContainerStyle={{ marginBottom: 64 }}
-    //   renderItem={({ item }) => <PhotoGridItem {...item} />}
-    //   keyExtractor={(item, index) => item.postId}
-    //   {...props}
-    // />
   );
 }
+
+export default withFirebaseHOC(PostList)

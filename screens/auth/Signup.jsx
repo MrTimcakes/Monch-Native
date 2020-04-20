ce0130addb4b812f1ea1e74ceb21dca845f69393
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-20 12:09:23 +0100

    Move to new signup screen

---
+++ screens/auth/Signup.jsx
@@ -0,0 +1,178 @@
+import React, { useState, useEffect } from 'react';
+import { 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  ImageBackground,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { LinearGradient } from 'expo-linear-gradient';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { withFirebaseHOC } from 'Monch/utilities/Firebase'
+
+import TitlePath from 'Monch/assets/SVG/TitlePath';
+import Colors from 'Monch/constants/Colors';
+
+import StyledInput from '../../components/StyledInput';
+import StyledSwitch from '../../components/StyledSwitch';
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .label('Username')
+    .required()
+    .min(5, 'Username must be atleast ${min} characters')
+    .max(256, `Woah, slow down there!`),
+  email: yup
+    .string()
+    .label('Email')
+    .email()
+    .required(),
+  password: yup
+    .string()
+    .label('Password')
+    .required()
+    .min(6, 'Seems a bit short...')
+    .max(256, 'There\'s no way you can remember that, try a shorter password.'),
+  confirmPassword: yup
+    .string()
+    .required()
+    .label('Confirm password')
+    .test('passwords-match', 'Passwords must match ya fool', function(value) {
+      return this.parent.password === value;
+    }),
+  agreeToTerms: yup
+    .boolean()
+    .label('Terms')
+    .test(
+      'is-true',
+      'Must agree to terms to continue',
+      value => value === true
+    ),
+});
+
+
+
+function Signup({firebase, navigation}){
+
+  const handleOnSignup = async (values, actions) => {
+    const { username, email, password } = values
+    console.log("Username => ", username)
+    console.log("email => ", email)
+    console.log("password => ", password)
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(result) {
+      const userData = {
+        badges: [],
+        bio: '',
+        cookingSkill: 0.0,
+        username: username,
+        uid: result.user.uid,
+        xp: 0.0,
+        followers: 0,
+        following: 0,
+      };
+      return firebase.createNewUser(userData)
+      .then(result => {
+        return result.user.updateProfile({
+          displayName: username
+        })
+        .then(()=>{
+          navigation.navigate('App')
+        })
+      })
+    }).catch(function(error) {
+      actions.setFieldError('general', error.message)
+    });
+  }
+  return (
+    <SafeAreaView>
+      <ImageBackground source={require('Monch/assets/images/SplitFruits.jpg')} style={{width: '100%', height: '100%'}}>
+        <LinearGradient colors={['rgba(0,0,0,0.3)', 'rgba(0,0,0,0.9)']} style={{ position: 'absolute', left: 0, right: 0, top: 0, height: '100%', }} />
+        
+        <ScrollView>
+          <View style={{alignItems: 'center'}}>
+            <TitlePath width='70%' />
+          </View>
+          <Formik
+            initialValues={{
+              username: '',
+              email: '',
+              password: '',
+              confirmPassword: '',
+              agreeToTerms: false,
+            }}
+            onSubmit={(values, actions) => {
+              handleOnSignup(values, actions)
+            }}
+            validationSchema={validationSchema}
+          >
+            {formikProps => (
+              <React.Fragment>
+                <StyledInput
+                  formikProps={formikProps}
+                  formikKey="username"
+                  placeholder="Username"
+                  autoFocus
+                />
+
+                <StyledInput
+                  formikProps={formikProps}
+                  formikKey="email"
+                  placeholder="E-mail"
+                />
+
+                <StyledInput
+                  formikProps={formikProps}
+                  formikKey="password"
+                  placeholder="Password"
+                  secureTextEntry
+                />
+
+                <StyledInput
+                  formikProps={formikProps}
+                  formikKey="confirmPassword"
+                  placeholder="Confirm Password"
+                  secureTextEntry
+                />
+
+                <StyledSwitch
+                  label="Agree to Terms"
+                  formikKey="agreeToTerms"
+                  formikProps={formikProps}
+                />
+
+                {formikProps.isSubmitting ? (
+                  <ActivityIndicator />
+                ) : (
+                  <View style={{alignItems: 'center'}}>
+                    <TouchableOpacity onPress={formikProps.handleSubmit} style={{ 
+                      backgroundColor: Colors.color2,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      width: '80%',
+                      height: 60,
+                      borderRadius: 10,
+                    }}><Text style={{fontFamily: 'sans-serif', fontWeight: 'normal', fontSize: 16, color: '#fff'}}>REGISTER</Text></TouchableOpacity>
+                  </View>
+                )}
+              </React.Fragment>
+            )}
+          </Formik>
+          <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 40}}>
+            <TouchableOpacity><Text style={{color:'#fff'}}>Already have an account? <Text style={{color: Colors.color1}}>Login</Text></Text></TouchableOpacity>
+          </View>
+        </ScrollView>
+
+      </ImageBackground>
+    </SafeAreaView>
+  );
+}
+
+export default withFirebaseHOC(Signup);

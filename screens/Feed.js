8753a37b441914ea0686a6f5e87abac867b004cd
Timothy Adamson <MrTimcakes@gmail.com>
2020-04-20 23:54:38 +0100

    Fetch from global list

--- screens/Feed.js
+++ screens/Feed.js
@@ -7,38 +7,87 @@ import {
   Image,
   TouchableOpacity,
   FlatList,
+  RefreshControl,
 } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import TimeAgo from 'javascript-time-ago';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Colors from '../constants/Colors';
+import NewProfilePostScreen from 'Monch/screens/NewProfilePost';
+import PostListScreen from 'Monch/screens/PostList';
 
 import MonchHeader from 'Monch/components/MonchHeader';
 import Post from 'Monch/components/Post';
+import Colors from '../constants/Colors';
 
 import { withFirebaseHOC } from '../utilities/Firebase'
 
-function FeedScreen(P) {
+const PAGE_SIZE = 5;
+
+function FeedScreen({firebase}) {
   const [feed, setFeed] = useState(null);
-  const [refreshing, setRefreshing] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [lastKnownKey, setLastKnownKey] = useState(0);
 
   useEffect(()=>{
-    fetchPosts('OS2GbNqiREVn3WBh3y0U6zvgg4w2'); // Temporary, fetch posts from me
+    fetchPosts();
   },[])
 
-  const fetchPosts = (uid) => {
-    P.firebase.firestore().collection("users").doc(uid).collection("posts").get().then(function(querySnapshot) {
+  // const fetchPosts = async lastKey => {
+  //   // If we are currently getting posts, then bail out..
+  //   if (refreshing) { return; }
+  //   setRefreshing(true);
+
+  //   // The data prop will be an array of posts, the cursor will be used for pagination.
+//   const { data, cursor } = await firebase.getPaged({
+  //     size: PAGE_SIZE,
+  //     start: lastKey
+  //   });
+
+  //   setLastKnownKey(cursor);
+  //   // Iteratively add posts
+  //   // let allPosts = [];
+  //   // if(feed){allPosts=[...feed]};
+  //   // console.log("All Posts => ", allPosts)
+  //   // for (let child of data) {
+  //   //   allPosts = [...allPosts, child]
+  //   // }
+  //   console.log("Data => ", JSON.stringify(data))
+
+  //   console.log("\n")
+  //   let allPosts = [];
+  //   if(feed){allPosts=[feed, data]};
+  //   setFeed(allPosts);
+
+  //   // Finish loading, this will stop the refreshing animation.
+  //   setRefreshing(false);
+  // }
+
+  // const onPressFooter = () => fetchPosts(lastKnownKey);
+
+  // const FooterComponent = () => {
+  //   return(
+  //     <View style={{alignItems: 'center'}}>
+  //       <TouchableOpacity onPress={onPressFooter}><Text>Load more!</Text></TouchableOpacity>
+  //     </View>
+  //   )
+  // }
+
+  useEffect(()=>{
+    fetchPosts(); // Temporary, fetch global posts
+  },[])
+
+  const fetchPosts = async () => {
+    firebase.firestore().collection('globalPosts').orderBy('timestamp', 'desc').limit(50).get().then(function(querySnapshot) {
+      // console.log(querySnapshot)
       let allPosts = [];
       querySnapshot.forEach(function(doc) { allPosts = [...allPosts, doc.data()]; });
+      // console.log(JSON.stringify(allPosts))
       setFeed(allPosts);
       setRefreshing(false);
+    })
+    .catch(function(error) {
+        console.log("Error getting document:", error);
     });
-  }
-
-  useEffect(() => { return P.navigation.addListener("MultiFuncPress", MuliFuncAction); }, [P.navigation]); // Add listener 
-  const MuliFuncAction = () => {
-    if( !P.navigation.isFocused() ){return} // If not focused do nothing
-    console.log("Feed MultiFuncPress");
   }
 
   return (
@@ -46,7 +95,7 @@ function FeedScreen(P) {
       <MonchHeader />
       {/* <Stories /> */}
       <FlatList
-        // ListFooterComponent={Stories}
+        // ListFooterComponent={FooterComponent}
         // ListFooterComponentStyle={{paddingBottom: 25}}
         // ListFooterComponentStyle={{ marginTop: -25 }}
         inverted={true}
@@ -56,6 +105,9 @@ function FeedScreen(P) {
         keyExtractor={item => item.postId}
         ListHeaderComponent={<View></View>}
         ListHeaderComponentStyle={{height:80}}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={fetchPosts} />
+        }
       />
     </SafeAreaView>
   );
@@ -71,4 +123,21 @@ const styles = StyleSheet.create({
   }
 })
 
-export default withFirebaseHOC(FeedScreen)
+const Stack = createStackNavigator();
+function FeedNavigator(P){
+  useEffect(() => { return P.navigation.addListener("MultiFuncPress", MuliFuncAction); }, [P.navigation]); // Add listener for MultiFunction Button
+  const MuliFuncAction = () => {
+    if( !P.navigation.isFocused() ){return} // If not focused do nothing
+    P.navigation.navigate("New Post");
+  }
+
+  return (
+    <Stack.Navigator initialRouteName="Feed">
+      <Stack.Screen name="Feed" component={withFirebaseHOC(FeedScreen)} options={{ headerShown: false }}/>
+      <Stack.Screen name="New Post" component={withFirebaseHOC(NewProfilePostScreen)} />
+      <Stack.Screen name="Post List" component={withFirebaseHOC(PostListScreen)} />
+    </Stack.Navigator>
+  );
+}
+
+export default withFirebaseHOC(FeedNavigator)
